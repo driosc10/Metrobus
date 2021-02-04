299b3f1be69aa5d779acd855fa89e952137f6e8e
Daniel Rios <daniel.rios@qacg.com>
2021-02-04 15:43:08 -0600

    add validations

--- Metrobus/Controllers/HomeController.cs
+++ Metrobus/Controllers/HomeController.cs
@@ -39,8 +39,8 @@ namespace Metrobus.Controllers
                 if (consulta > 0)
                 {
                     var urlMetrobusCDMX = "https://datos.cdmx.gob.mx/api/3/action/datastore_search?resource_id=ad360a0e-b42f-482c-af12-1fd72140032e";
-                    var urlMetrobusByID = "https://localhost:44325/api/Metrobus/id?id=";
-                    var urlInserMetrobus = "https://localhost:44325/api/Metrobus";
+                    var urlMetrobusByID = "http://localhost:8080/api/Metrobus/id?id=";
+                    var urlInserMetrobus = "http://localhost:8080/api/Metrobus";
 
                     using (var http = new HttpClient())
                     {
@@ -61,25 +61,10 @@ namespace Metrobus.Controllers
                                 metrobus.Date_Update = item["date_updated"];
                                 metrobus.Latitud = item["position_latitude"];
                                 metrobus.Longitud = item["position_longitude"];
-
-                                var metrobusFindJSON = await http.GetStringAsync(urlMetrobusByID + metrobus.Vehicle_ID.ToString());
-
-                                if (!string.IsNullOrEmpty(metrobusFindJSON))
-                                {
-                                    var metrobusFind = JsonConvert.DeserializeObject<MetrobusesModel>(metrobusFindJSON);
-                                    if (metrobusFind.Vehicle_ID != metrobus.Vehicle_ID && metrobusFind.Latitud != metrobus.Latitud && metrobusFind.Longitud != metrobus.Longitud && metrobusFind.Date_Update != metrobus.Date_Update)
-                                    {
-                                        var json = JsonConvert.SerializeObject(metrobus);
-                                        var metrobusNew = new StringContent(json, Encoding.UTF8, "application/json");
-                                        var insertMetrobus = await http.PostAsync(urlInserMetrobus, metrobusNew);
-                                    }
-                                }
-                                else
-                                {
-                                    var json = JsonConvert.SerializeObject(metrobus);
-                                    var metrobusNew = new StringContent(json, Encoding.UTF8, "application/json");
-                                    var insertMetrobus = await http.PostAsync(urlInserMetrobus, metrobusNew);
-                                }
+                                var json = JsonConvert.SerializeObject(metrobus);
+                                var metrobusNew = new StringContent(json, Encoding.UTF8, "application/json");
+                                var insertMetrobus = await http.PostAsync(urlInserMetrobus, metrobusNew);
+                                
                             }
                         }
                         ViewBag.ErrorMessage = "La información se consulto de forma correcta";
@@ -92,7 +77,7 @@ namespace Metrobus.Controllers
             }
             finally
             {
-                var urlMetrobus = "https://localhost:44325/api/Metrobus";
+                var urlMetrobus = "http://localhost:8080/api/Metrobus";
                 using (var http = new HttpClient())
                 {
                     var response = await http.GetStringAsync(urlMetrobus);
@@ -114,7 +99,7 @@ namespace Metrobus.Controllers
             string id = HttpContext.Session.GetString("VehicleID");
             if (!string.IsNullOrEmpty(id))
             {
-                var url = "https://localhost:44325/api/Metrobus/id?id=" + id.ToString();
+                var url = "http://localhost:8080/api/Metrobus/id?id=" + id.ToString();
 
                 using (var http = new HttpClient())
                 {
@@ -127,15 +112,154 @@ namespace Metrobus.Controllers
         public async Task<IActionResult> Alcaldias()
         {
             HttpContext.Session.Clear();
-            var url = "https://localhost:44325/api/Alcaldias";
+            var url = "http://localhost:8080/api/Alcaldias";
 
             using(var http =  new HttpClient())
             {
                 var response = await http.GetStringAsync(url);
-                ViewBag.alcaldias = JsonConvert.DeserializeObject<List<AlcaldiasModel>>(response);
+                var alcaldias = JsonConvert.DeserializeObject<List<AlcaldiasModel>>(response);
+
+                if(alcaldias.Count() == 0)
+                {
+                    List<AlcaldiasModel> alcaldiasList = new List<AlcaldiasModel>();
+                    alcaldiasList = GetAlcaldias();
+
+                    foreach (var alcadia in alcaldiasList)
+                    {
+                        var json = JsonConvert.SerializeObject(alcadia);
+                        var alcadiaNew = new StringContent(json, Encoding.UTF8, "application/json");
+                        var insertAlcadia = await http.PostAsync(url, alcadiaNew);
+                    }
+
+                    var response2 = await http.GetStringAsync(url);
+                    var alcaldias2 = JsonConvert.DeserializeObject<List<AlcaldiasModel>>(response);
+                    ViewBag.alcaldias = alcaldias2;
+                }
+                else
+                {
+                    ViewBag.alcaldias = alcaldias;
+                }
             }
 
             return View();
+        }
+
+        private List<AlcaldiasModel> GetAlcaldias()
+        {
+            List<AlcaldiasModel> alcaldiasList = new List<AlcaldiasModel>();
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Milpa Alta",
+                Latitud = "19.1394565999",
+                Longitud = "-99.0510954218"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Benito Juárez",
+                Latitud = "19.3806424162",
+                Longitud = "-99.1611346584"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Gustavo A. Madero",
+                Latitud = "19.5040652077",
+                Longitud = "-99.1158642087"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Coyoacan",
+                Latitud = "19.3266672536",
+                Longitud = "-99.1503763525"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Miguel Hidalgo",
+                Latitud = "19.4280623649",
+                Longitud = "-99.2045669144"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "La Magdalena Contreras",
+                Latitud = "19.2689765031",
+                Longitud = "-99.2684129061"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Tláhuac",
+                Latitud = "19.2769983772",
+                Longitud = "-99.0028216137"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Azcapotzalco",
+                Latitud = "19.4853286147",
+                Longitud = "-99.1821069423"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Iztacalco",
+                Latitud = "19.396911897",
+                Longitud = "-99.094329797"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Álvaro Obregón",
+                Latitud = "19.336175562",
+                Longitud = "-99.246819712"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Xochimilco",
+                Latitud = "19.2451450458",
+                Longitud = "-99.0903636045"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Venustiano Carranza",
+                Latitud = "19.4304954545",
+                Longitud = "-99.0931057959"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Tlalpan",
+                Latitud = "19.1983396763",
+                Longitud = "-99.2062207957"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Cuajimalpa de Morelos",
+                Latitud = "19.3246343001",
+                Longitud = "-99.3107285253"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Cuauhtémoc",
+                Latitud = "19.4313734294",
+                Longitud = "-99.1490557562"
+            });
+
+            alcaldiasList.Add(new AlcaldiasModel
+            {
+                Name = "Iztapalapa",
+                Latitud = "19.3491663204",
+                Longitud = "-99.0567989642"
+            });
+
+            return alcaldiasList;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
